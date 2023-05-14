216c205a5d2ba57483fd3c75a090bdbf992d6792
TEII RI <teiiri@outlook.com>
2023-05-14 23:59:30 +0100

    Update project demo

--- example/demo.c
+++ example/demo.c
@@ -18,19 +18,39 @@ static Bitmap *gr_checkerboard(int32_t size, uint16_t color_depth) {
     return bmp;
 }
 
+static Bitmap *gradient_patten(int32_t size, uint16_t color_depth) {
+    Bitmap *bmp = (Bitmap *) malloc(sizeof(Bitmap));
+    bmp_new(bmp, size, size, color_depth);
+
+    double step = 256.0 / size;
+
+    for (int32_t i = 0; i < size; i++)
+        for (int32_t j = 0; j < size; j++) {
+            uint8_t r = (uint8_t) (j * step),
+                    g = (uint8_t) ((i + j) * step / 2),
+                    b = (uint8_t) (i * step);
+            bmp_set_pixel(bmp, i, j, r, g, b);
+        }
+
+    return bmp;
+}
+
 int main() {
     Bitmap *patten;
 
-    printf("gr_checkerboard(64, 24)\n");
-    patten = gr_checkerboard(64, 24);
+    int32_t size = 256;
+    uint16_t color_depth = 24;
+
+    printf("\n1. gr_checkerboard(%d, %d)\n", size, color_depth);
+    patten = gr_checkerboard(size, color_depth);
     bmp_print_header(patten);
-    bmp_write_file(patten, "gr_checkerboard_64_24.bmp");
+    bmp_write_file(patten, "gr_checkerboard.bmp");
     bmp_free(patten);
 
-    printf("\ngr_checkerboard(256, 32)\n");
-    patten = gr_checkerboard(256, 32);
+    printf("\n2. gradient_patten(%d, %d)\n", size, color_depth);
+    patten = gradient_patten(size, color_depth);
     bmp_print_header(patten);
-    bmp_write_file(patten, "gr_checkerboard_256_32.bmp");
+    bmp_write_file(patten, "gradient_patten.bmp");
     bmp_free(patten);
 
     return 0;
